9393ea333d125b27587f76406c1cdb3d3f3bf391
mariamiah <mariah.kakande@gmail.com>
2019-10-04 06:57:18 +0300

    ft(human): add moves to play with fellow human [Delivers #168939737]

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 from main.computer import Computer
+from main.human import Human
 
 computer = Computer()
+human = Human()
+first_player = 'X'
+turn = 1
 def human_computer():
     computer.play_game()
 def human_human():
-    pass
+    print('\n\nWelcome to Tic Tac Toe for two humans!')
+    human.print_board(initial=True)
+    human.play(first_player, turn)
 
 def main():
     computer.welcome_message()
